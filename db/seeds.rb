e6da6f1d9b010fcbbc5c345f624a739429c75da1
Jevgeni Krais <beichu@gmail.com>
2013-05-07 13:50:53 +0300

    added some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Items.create([{item_category: 'Something', item_name: 'Name 1', item_description: 'Desc 1', item_available: true},
+	{item_category: 'Something', item_name: 'Name 2', item_description: 'Desc 2', item_available: true}])
+Options.create([{option_price: 1, option_quantity: 9000, option_name: 'Blue', option_description: 'Some blue t-shirt', item_id: 1},
+	{option_price: 9000, option_quantity: 1, option_name: 'Red', option_description: 'Some red t-shirt', item_id: 2}])
