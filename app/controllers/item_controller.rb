93a2713a4919ade26e4434dbd4e58f03b71bcbb5
Jevgeni Krais <beichu@gmail.com>
2013-05-08 17:16:20 +0300

    fixed deprecation in ItemContoller, optimized (?) results

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -2,20 +2,54 @@ class ItemController < ApplicationController
 
 	def all
 		items = Array.new
-		Items.all.each do |item|
+		Items.find_each do |item|
 			option = Options.find(item.id)
-			item['option'] = option
-			items.push(item)
+			item_ = 
+			{
+				'id' => item.id,
+				'name' => item.item_name,
+				'description' => item.item_description,
+				'available' => item.item_available,
+				'category' => item.item_category,
+				'options' => {
+					'item_id' => option.item_id,
+					'description' => option.option_description,
+					'name' => option.option_name,
+					'price' => option.option_price,
+					'quantity' => option.option_quantity
+				}
+			}
+			items.push(item_)
 		end
 		render :json => items
 	end
 
 	def by_category
 		items = Array.new
-		Items.where("item_category = ?", params[:category]).each do |item|
-			option = Options.find(item.id)
-			item['option'] = option
-			items.push(item)
+		options = Options.all
+		Items.find_each do |item|
+			if item.item_category == params[:category]
+				options.each do |option|
+					if item.id == option.item_id
+						item_ = 
+						{
+							'id' => item.id,
+							'name' => item.item_name,
+							'description' => item.item_description,
+							'available' => item.item_available,
+							'category' => item.item_category,
+							'options' => {
+								'item_id' => option.item_id,
+								'description' => option.option_description,
+								'name' => option.option_name,
+								'price' => option.option_price,
+								'quantity' => option.option_quantity
+							}
+						}
+						items.push(item_)
+					end
+				end
+			end
 		end
 		render :json => items
 	end
